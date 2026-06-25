5dbfbc880682901183adafb44c907bb0dc2c4592
Jonathan Waddingham <jon_pjw@hotmail.com>
2026-06-25 08:57:42 +0100

    Configure non-interactive linting and audit policy

---
+++ .eslintrc.cjs
@@ -0,0 +1,39 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: false,
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
+  },
+  ignorePatterns: [
+    '.next/',
+    'out/',
+    'build/',
+    'next-env.d.ts',
+    'docs/Magic Patterns design/',
+    'scripts/generate-icons.js',
+  ],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-object-type': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    'no-var': 'off',
+  },
+};
